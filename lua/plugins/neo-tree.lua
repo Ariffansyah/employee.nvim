f6825563c2f1f1994c97f0cb05e77b9f69bd592b
Mohammad Ariffansyah <harimato9@gmail.com>
2026-01-11 09:52:11 +0700

    feat(neo-tree): add floating window, file size, and git symbols Enhance neo-tree with a floating window layout, display file sizes, and show custom git status symbols. Adjust file renderer for improved visuals and update plugin lockfile for dependencies.

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -1,11 +1,56 @@
 return {
   "nvim-neo-tree/neo-tree.nvim",
   opts = {
+    window = {
+      position = "float",
+      popup = {
+        size = {
+          height = "80%",
+          width = "20%",
+        },
+        position = "50%",
+      },
+    },
+    default_component_configs = {
+      file_size = {
+        enabled = true,
+        required_width = 1,
+        width = 12,
+      },
+      name = {
+        trailing_slash = false,
+        use_git_status_colors = true,
+        highlight = "NeoTreeFileName",
+      },
+      git_status = {
+        symbols = {
+          added = "✚",
+          modified = "",
+          deleted = "✖",
+          renamed = "󰁕",
+          untracked = "",
+          ignored = "",
+          unstaged = "󰄱",
+          staged = "",
+          conflict = "",
+          unmerged = "",
+        },
+      },
+    },
+    filesystem = {
+      renderers = {
+        file = {
+          { "icon" },
+          { "name",      use_git_status_colors = true },
+          { "git_status" },
+          { "file_size" },
+        },
+      },
+    },
     event_handlers = {
       {
         event = "file_opened",
         handler = function()
-          -- Auto close neo-tree when opening a file
           require("neo-tree.command").execute({ action = "close" })
         end,
       },
